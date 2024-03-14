97fdddb28fddca8b4364f7fea8993da6264fe655
Honey Patel <honeydpatel2523@gmail.com>
2024-03-14 14:50:23 +0530

    feat: add verification email for authenticating user

--- actions/login.ts
+++ actions/login.ts
@@ -1,21 +1,44 @@
 "use server";
 
 import * as z from "zod";
-import { LoginSchema } from "@/schemas";
-import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
+
+import { signIn } from "@/auth";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import { sendVerificationEmail } from "@/lib/mail";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-  
+  const validatedFields = LoginSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Email does not exist!" }
+  }
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email,
+    );
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token,
+    );
+
+    return { success: "Confirmation email sent!" };
+  }
+
   try {
-    const validatedFields = LoginSchema.safeParse(values);
-  
-    if (!validatedFields.success) {
-      return { error: "Invalid fields!" };
-    }
-  
-    const { email, password } = validatedFields.data;
     await signIn("credentials", {
       email,
       password,
--- components/auth/loginForm.tsx
+++ components/auth/loginForm.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
     Form,
     FormControl,
@@ -11,38 +11,46 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { CardWrapper } from "./card-wrapper"
-import { LoginSchema } from "@/schemas"
-import { login } from "@/actions/login"
-import { startTransition, useState } from "react"
-
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { CardWrapper } from "./card-wrapper";
+import { LoginSchema } from "@/schemas";
+import { login } from "@/actions/login";
+import { useSearchParams } from "next/navigation";
+import { useState, useTransition } from "react";
+import Link from "next/link";
 
 export function LoginForm() {
+    const searchParams = useSearchParams();
+    const urlError =
+        searchParams.get("error") === "OAuthAccountNotLinked"
+            ? "Email already in use with different provider!"
+            : "";
+
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
+
+    const [isPending, startTransition] = useTransition();
 
     const form = useForm<z.infer<typeof LoginSchema>>({
         resolver: zodResolver(LoginSchema),
         defaultValues: {
             email: "",
-            password: ""
+            password: "",
         },
-    })
+    });
 
     const onSubmit = (values: z.infer<typeof LoginSchema>) => {
         setError("");
         setSuccess("");
 
         startTransition(() => {
-            login(values)
-                .then((data) => {
-                    setError(data?.error);
-                    setSuccess(data?.success);
-                });
+            login(values).then((data) => {
+                setError(data?.error);
+                setSuccess(data?.success);
+            });
         });
-    }
+    };
 
     return (
         <CardWrapper
@@ -60,7 +68,7 @@ export function LoginForm() {
                             <FormItem>
                                 <FormLabel>Email</FormLabel>
                                 <FormControl>
-                                    <Input placeholder="email" {...field} />
+                                    <Input disabled={isPending} placeholder="email" {...field} />
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -73,17 +81,32 @@ export function LoginForm() {
                             <FormItem>
                                 <FormLabel>Passwrod</FormLabel>
                                 <FormControl>
-                                    <Input placeholder="******" {...field} />
+                                    <Input
+                                        disabled={isPending}
+                                        placeholder="******"
+                                        type="password"
+                                        {...field}
+                                    />
                                 </FormControl>
+                                <Button
+                                    size="sm"
+                                    variant="link"
+                                    asChild
+                                    className="px-0 font-normal"
+                                >
+                                    <Link href="/auth/reset">Forgot password?</Link>
+                                </Button>
                                 <FormMessage />
                             </FormItem>
                         )}
                     />
-                    <Button className="w-full" type="submit">Submit</Button>
+                    <p>{error || urlError} </p>
+                    <p>{success}</p>
+                    <Button disabled={isPending} className="w-full" type="submit">
+                        Submit
+                    </Button>
                 </form>
-                <p>{error}</p>
-                <p>{success}</p>
             </Form>
         </CardWrapper>
-    )
+    );
 }
